cdc6464a575cb591ebd63c79a6bd619315edce95
Mairon.az <mairon.costa@azi.com.br>
2024-12-31 09:43:06 -0400

    update intermediate

---
+++ usuarios/models/intermediate/int_ft_acessos_legado.sql
@@ -0,0 +1,23 @@
+WITH source AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY dl.id) AS FT_SK,
+        us.sk_dim AS SK_DIM_USUARIOS,
+        st.sk_dim AS SK_DIM_SISTEMAS,
+        ng.sk_dim AS SK_DIM_NAVEGADOR,
+        ac.sk_dim AS SK_DIM_ACAO,
+        dl.coluna_ativa_de,
+        dl.coluna_ativa_ate
+    FROM
+        {{ ref('stg_acessos_legado') }} dl
+    INNER JOIN {{ ref('int_dim_usuarios') }} us ON us.COD_USUARIO = dl.id_usuario
+    INNER JOIN {{ ref('int_dim_sistemas') }} st ON st.DESC_SISTEMAS = dl.sistema_origem
+    INNER JOIN {{ ref('int_dim_navegador') }} ng ON ng.DESC_NAVEGADOR = dl.navegador
+    INNER JOIN {{ ref('int_dim_acao') }} ac ON ac.DESC_ACAO = dl.acao_realizada
+
+    ORDER BY id ASC
+)
+
+SELECT
+    *
+FROM
+    source
