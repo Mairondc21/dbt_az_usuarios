e3152b0a26db06d33b44e6e25fe941c61af6593f
Mairon.az <mairon.costa@azi.com.br>
2024-12-31 11:01:37 -0400

    update

--- usuarios/models/staging/stg_acessos_legado.sql
+++ usuarios/models/staging/stg_acessos_legado.sql
@@ -6,10 +6,28 @@ WITH source AS (
         sistema_origem,
         navegador,
         acao_realizada,
-        dbt_valid_from AS coluna_ativa_de,
-        dbt_valid_to AS coluna_ativa_ate
+        dbt_valid_from,
+        dbt_valid_to
     FROM
         {{ ref('usuario_snapshot') }}
+),
+transform_date AS (
+    SELECT
+        *,
+        to_char(dbt_valid_from, 'YYYY-MM-DD HH24:MI:SS') AS coluna_ativa_de,
+        to_char(dbt_valid_TO, 'YYYY-MM-DD HH24:MI:SS') AS coluna_ativa_ate
+    FROM
+        source
 )
 
-SELECT * FROM source
+SELECT
+    id,
+    id_usuario,
+    nome_usuario,
+    sistema_origem,
+    navegador,
+    acao_realizada,
+    coluna_ativa_de,
+    coluna_ativa_ate
+FROM 
+    transform_date
