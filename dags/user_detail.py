f56f56153481a8badb0806ffb9a7d5b532c357e3
Mairon.az <duartemairon@gmail.com>
2025-01-04 22:57:13 -0400

    DAGs funcionando

--- dags/user_detail.py
+++ dags/user_detail.py
@@ -1,18 +1,44 @@
-from datetime import datetime
+from datetime import datetime,timedelta
+
+from airflow import DAG
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
 
 from cosmos import DbtDag, ProjectConfig
 
+from airflow.models.baseoperator import chain
+
 from include.profiles import airflow_db
-from include.constants import user_path, venv_execution
+from include.constants import user_path
 
-simple_dag = DbtDag(
-    project_config=ProjectConfig(user_path),
-    profile_config=airflow_db,
-    execution_config=venv_execution,
-    schedule_interval=None,
-    start_date=datetime(2024,10,10),
+with DAG(
+    dag_id="example_airbyte_operator",
+    schedule=None,
+    start_date=datetime(2021, 1, 1),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["example"],
     catchup=False,
-    dag_id="user_detail",
-    tags=["detail"],
+) as dag:
+    
+    sync_source_destination = AirbyteTriggerSyncOperator(
+        task_id="airbyte_sync_source_dest_example",
+        airbyte_conn_id="airbyte_conn",
+        connection_id='80fc48a7-a9bb-48eb-aeb4-ed97565bbb99',
+    )
 
-)
+
+    async_source_destination = AirbyteTriggerSyncOperator(
+        task_id="airbyte_async_source_dest_example",
+        connection_id='80fc48a7-a9bb-48eb-aeb4-ed97565bbb99',
+        asynchronous=True,
+    )
+    airbyte_sensor = AirbyteJobSensor(
+        task_id="airbyte_sensor_source_dest_example",
+        airbyte_job_id=async_source_destination.output,
+    )
+
+
+
+    async_source_destination >> airbyte_sensor
+
